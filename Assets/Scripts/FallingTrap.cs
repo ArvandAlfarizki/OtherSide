d3abf19c0c0b73a6e7f6b7724e296ac306fffc4c
ArvandAlfarizki <arvand.rizki01@gmail.com>
2021-11-24 11:56:12 +0700

    Stage1-BetaTest

---
+++ Assets/Scripts/FallingTrap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingTrap : MonoBehaviour
+{
+    Rigidbody2D Rb;
+    [SerializeField]
+    float Speed;
+    [SerializeField]
+    LayerMask HeroLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Physics2D.Raycast(Rb.position , Vector2.down, 10f , HeroLayer))
+        {
+            Rb.velocity = Vector2.down * Speed;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            collision.gameObject.GetComponent<PlayerMovement>().Death();
+        }
+    }
+}
