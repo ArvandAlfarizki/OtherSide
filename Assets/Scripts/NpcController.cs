98382fd1246e3d94a3a80f84d6f433e3d297ded6
ArvandAlfarizki <arvand.rizki01@gmail.com>
2021-11-25 18:56:40 +0700

    FixedUpdateStage1

--- Assets/Scripts/NpcController.cs
+++ Assets/Scripts/NpcController.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NpcController : MonoBehaviour
 {
     public bool isAccept;
+    [SerializeField]
+    private Text pickupText;
     //public Animator animator;
     // Start is called before the first frame update
+    private void Start()
+    {
+        pickupText.gameObject.SetActive(false);
+    }
     public void Accept()
     {
         if(!isAccept)
@@ -14,6 +21,7 @@ public class NpcController : MonoBehaviour
             isAccept = true;
             Debug.Log("Accepted");
             //animator.SetBool("Accept", isAccept);
+            pickupText.gameObject.SetActive(true);
         }
     }
 }
