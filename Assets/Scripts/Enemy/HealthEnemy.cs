1803b05ff90565c58d02ab0b798f8deca19b7f89
ArvandAlfarizki <arvand.rizki01@gmail.com>
2021-12-03 19:21:49 +0700

    UpdateOtherSide V0.2

---
+++ Assets/Scripts/Enemy/HealthEnemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthEnemy : MonoBehaviour
+{
+    public float startHealth;
+    private float hp;
+
+    public GameObject dieEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = startHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float damage)
+    {
+        hp -= damage;
+
+        if(hp <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        if(dieEffect !=null)
+        {
+            Instantiate(dieEffect, transform.position, Quaternion.identity);
+        }
+        Destroy(gameObject);
+    }
+}
