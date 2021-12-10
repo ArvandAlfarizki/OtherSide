225c74212dcadc41e0aab1dfd6a703e93ff1f52c
Arvand Alfarizki Pratama <90150213+ArvandAlfarizki@users.noreply.github.com>
2021-12-10 11:52:09 +0700

    Update Playable

--- Assets/Scripts/Trap/FallingTrap.cs
+++ Assets/Scripts/Trap/FallingTrap.cs
@@ -24,11 +24,5 @@ public class FallingTrap : MonoBehaviour
             Rb.velocity = Vector2.down * Speed;
         }
     }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.gameObject.CompareTag("Player"))
-        {
-            collision.gameObject.GetComponent<PlayerMovement>().Death();
-        }
-    }
+    
 }
