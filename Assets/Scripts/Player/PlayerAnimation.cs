54696e81b6b0000aa44f00825701cfa99f63e4d3
Arvand Alfarizki Pratama <90150213+ArvandAlfarizki@users.noreply.github.com>
2022-01-07 14:42:31 +0700

    Final Update

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -1,71 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< Updated upstream
-using DragonBones;
-
-public class PlayerAnimation : MonoBehaviour
-{
-    public string idleAnimation = "idle";
-	public string runAnimation = "run";
-    public string jumpAnimation = "jump";
-    public string dieAnimation = "death";
-
-    enum State {IDLE, RUN, JUMP, DEATH};
-    private State state = State.IDLE;
-    private UnityArmatureComponent armatureComponent;
-    // Start is called before the first frame update
-    void Start()
-    {
-        armatureComponent = transform.GetComponentInChildren<UnityArmatureComponent>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void Idle() {
-		if (state != State.DEATH && state != State.IDLE) {
-			armatureComponent.animation.FadeIn (idleAnimation, 0.5f, -1);
-			armatureComponent.animation.timeScale = 1f;
-			state = State.IDLE;
-		}
-	}
-
-    public void Run(float speed) {
-		if (state != State.DEATH) {
-			if (speed > 0 && transform.lossyScale.x > 0 || speed < 0 && transform.lossyScale.x < 0) {
-				if (state != State.RUN) {
-					armatureComponent.animation.FadeIn (runAnimation, 0.25f, -1);
-					state = State.RUN;
-				}
-				armatureComponent.animation.timeScale = speed;
-			} else if (speed < 0 && transform.lossyScale.x > 0 || speed > 0 && transform.lossyScale.x < 0) {
-				if (state != State.RUN) {
-					armatureComponent.animation.FadeIn (runAnimation, 0.25f, -1);
-					state = State.RUN;
-				}
-				armatureComponent.animation.timeScale = -speed;
-			}
-		}
-	}
-
-	public void Jump() {
-		if (state != State.DEATH && state != State.JUMP) {
-			armatureComponent.animation.FadeIn (jumpAnimation, 0.25f, 1);
-			armatureComponent.animation.timeScale = 1f;
-			state = State.JUMP;
-		}
-	}
-
-    public void Death() {
-		if (state != State.DEATH) {
-			armatureComponent.animation.Play (dieAnimation, 1);
-			armatureComponent.animation.timeScale = 1f;
-			state = State.DEATH;
-		}
-=======
 
 public class PlayerAnimation : MonoBehaviour
 {
@@ -129,6 +64,5 @@ public class PlayerAnimation : MonoBehaviour
 			localScale.x *= -1;
 
 		transform.localScale = localScale;
->>>>>>> Stashed changes
 	}
 }
--- Assets/Scripts/Player/PlayerMovementAction.cs
+++ Assets/Scripts/Player/PlayerMovementAction.cs
@@ -2,40 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
-<<<<<<< Updated upstream
-
-public class PlayerMovementAction : MonoBehaviour
-{
-
-    public GameObject gameOverText, restartButton;
-    public float MovementSpeed = 1;
-    public float JumpForce = 1;
-    private Rigidbody2D rigidBody;
-    AudioSource audioSrc;
-    bool isMoving = false;
-    
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-        gameOverText.SetActive (false);
-        restartButton.SetActive (false);
-
-        rigidBody = GetComponent<Rigidbody2D>();
-        audioSrc = GetComponent<AudioSource> ();
-        
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-
-        float movement = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
-
-        if (!Mathf.Approximately(0, movement))
-        transform.rotation = movement < 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
-=======
 using DragonBones;
 
 public class PlayerMovementAction : MonoBehaviour
@@ -89,34 +55,10 @@ public class PlayerMovementAction : MonoBehaviour
         else{
         isMoving = false;
         }
->>>>>>> Stashed changes
 
         if (isMoving) {
             if (!audioSrc.isPlaying)
             audioSrc.Play ();
-<<<<<<< Updated upstream
-        }
-        else   {
-            audioSrc.Stop ();
-
-        }
-        
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(rigidBody.velocity.y) < 0.001f)
-        {
-            rigidBody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
-        }
-    }
-
-    void OnCollisionEnter2D (Collision2D col)
-    {
-        if (col.gameObject.tag.Equals ("Enemy"))
-        {
-        gameOverText.SetActive (true);
-        restartButton.SetActive (true);
-        gameObject.SetActive (false);
-        }
-    }
-=======
             player.animation.FadeIn(("run"), 0.10f, 1);
         }
         else if (isMoving)
@@ -150,5 +92,4 @@ public class PlayerMovementAction : MonoBehaviour
     }
 
     
->>>>>>> Stashed changes
 }
